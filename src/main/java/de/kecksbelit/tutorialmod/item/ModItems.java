bea65a3bc0f09e386dc1c3c3c361b78a97f02a22
KecksbeLi <89603402+KecksbeLi@users.noreply.github.com>
2023-05-25 22:46:15 +0200

    "1 Added ZirconBlock"

--- src/main/java/de/kecksbelit/tutorialmod/item/ModItems.java
+++ src/main/java/de/kecksbelit/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.kecksbelit.tutorialmod.item;
 
 import de.kecksbelit.tutorialmod.Tutorialmod;
+import de.kecksbelit.tutorialmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,6 +13,7 @@ import java.util.function.Supplier;
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Tutorialmod.MOD_ID);
     public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",() -> new Item(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB)));
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",() -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TUTORIAL_TAB).stacksTo(1)));
 
     public static void register(IEventBus eventBus)
     {
---
+++ src/main/java/de/kecksbelit/tutorialmod/item/custom/EightBallItem.java
@@ -0,0 +1,37 @@
+package de.kecksbelit.tutorialmod.item.custom;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+
+public class EightBallItem extends Item {
+    public EightBallItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+
+        if(!pLevel.isClientSide() && pUsedHand == InteractionHand.MAIN_HAND)
+        {
+            randomNumberOutput(pPlayer);
+            pPlayer.getCooldowns().addCooldown(this, 20);
+        }
+        return super.use(pLevel, pPlayer, pUsedHand);
+    }
+
+    private void randomNumberOutput(Player player)
+    {
+        player.sendSystemMessage(Component.literal("Your Number is" + getRandomNumber()));
+    }
+
+    private int getRandomNumber()
+    {
+        return RandomSource.createNewThreadLocalInstance().nextInt(10);
+    }
+}
